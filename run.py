32a728e1afd0381b7ffba373e54d635aa5843c07
Ildar Lomov <ildar.lomov@ostrovok.ru>
2018-11-13 15:40:24 +0300

    add some updates to work with video

--- run.py
+++ run.py
@@ -15,7 +15,6 @@ import cv2
 from stuff.helper import FPS2, WebcamVideoStream
 from skimage import measure
 
-
 ## LOAD CONFIG PARAMS ##
 if (os.path.isfile('config.yml')):
     with open("config.yml", 'r') as ymlfile:
@@ -24,20 +23,21 @@ else:
     with open("config.sample.yml", 'r') as ymlfile:
         cfg = yaml.load(ymlfile)
 
-VIDEO_INPUT     = cfg['video_input']
-FPS_INTERVAL    = cfg['fps_interval']
-ALPHA           = cfg['alpha']
-MODEL_NAME      = cfg['model_name']
-MODEL_PATH      = cfg['model_path']
-DOWNLOAD_BASE   = cfg['download_base']
-BBOX            = cfg['bbox']
-MINAREA         = cfg['minArea']
+VIDEO_INPUT = cfg['video_input']
+FPS_INTERVAL = cfg['fps_interval']
+ALPHA = cfg['alpha']
+MODEL_NAME = cfg['model_name']
+MODEL_PATH = cfg['model_path']
+DOWNLOAD_BASE = cfg['download_base']
+BBOX = cfg['bbox']
+MINAREA = cfg['minArea']
 
 # Hardcoded COCO_VOC Labels
 LABEL_NAMES = np.asarray([
     '', 'aeroplane', 'bicycle', 'bird', 'boat', 'bottle', 'bus',
     'car', 'cat', 'chair', 'cow', 'diningtable', 'dog', 'horse', 'motorbike',
     'person', 'pottedplant', 'sheep', 'sofa', 'train', 'tv'])
+
 
 def create_colormap(seg_map):
     """
@@ -53,6 +53,7 @@ def create_colormap(seg_map):
         ind >>= 3
     return colormap[seg_map]
 
+
 # Download Model from TF-deeplab's Model Zoo
 def download_model():
     model_file = MODEL_NAME + '.tar.gz'
@@ -62,17 +63,19 @@ def download_model():
         opener.retrieve(DOWNLOAD_BASE + model_file, model_file)
         tar_file = tarfile.open(model_file)
         for file in tar_file.getmembers():
-          file_name = os.path.basename(file.name)
-          if 'frozen_inference_graph.pb' in file_name:
-            tar_file.extract(file, os.getcwd() + '/models/')
+            file_name = os.path.basename(file.name)
+            if 'frozen_inference_graph.pb' in file_name:
+                tar_file.extract(file, os.getcwd() + '/models/')
         os.remove(os.getcwd() + '/' + model_file)
     else:
         print('> Model found. Proceed.')
 
+
 # Visualize Text on OpenCV Image
-def vis_text(image,string,pos):
-    cv2.putText(image,string,(pos),
-        cv2.FONT_HERSHEY_SIMPLEX, 0.75, (77, 255, 9), 2)
+def vis_text(image, string, pos):
+    cv2.putText(image, string, (pos),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.75, (77, 255, 9), 2)
+
 
 # Load frozen Model
 def load_frozenmodel():
@@ -87,26 +90,28 @@ def load_frozenmodel():
     return detection_graph
 
 
-def segmentation(detection_graph,label_names):
+def segmentation(detection_graph, label_names):
     # fixed input sizes as model needs resize either way
-    vs = WebcamVideoStream(VIDEO_INPUT,640,480).start()
-    resize_ratio = 1.0 * 513 / max(vs.real_width,vs.real_height)
+    # vs = WebcamVideoStream(VIDEO_INPUT, 640, 480).start()
+    vs = WebcamVideoStream('../Facial_landmark_detection_test/test/data/short.mp4', 1280, 720).start()
+    resize_ratio = 1.0 * 513 / max(vs.real_width, vs.real_height)
     target_size = (int(resize_ratio * vs.real_width), int(resize_ratio * vs.real_height))
     config = tf.ConfigProto(allow_soft_placement=True)
-    config.gpu_options.allow_growth=True
+    config.gpu_options.allow_growth = True
     fps = FPS2(FPS_INTERVAL).start()
     print("> Starting Segmentaion")
     with detection_graph.as_default():
         with tf.Session(graph=detection_graph) as sess:
             while vs.isActive():
-                image = cv2.resize(vs.read(),target_size)
+                print(target_size)
+                image = cv2.resize(vs.read(), target_size)
                 batch_seg_map = sess.run('SemanticPredictions:0',
-                                feed_dict={'ImageTensor:0': [cv2.cvtColor(image, cv2.COLOR_BGR2RGB)]})
+                                         feed_dict={'ImageTensor:0': [cv2.cvtColor(image, cv2.COLOR_BGR2RGB)]})
                 # visualization
                 seg_map = batch_seg_map[0]
                 seg_image = create_colormap(seg_map).astype(np.uint8)
-                cv2.addWeighted(seg_image,ALPHA,image,1-ALPHA,0,image)
-                vis_text(image,"fps: {}".format(fps.fps_local()),(10,30))
+                cv2.addWeighted(seg_image, ALPHA, image, 1 - ALPHA, 0, image)
+                vis_text(image, "fps: {}".format(fps.fps_local()), (10, 30))
                 # boxes (ymin, xmin, ymax, xmax)
                 if BBOX:
                     map_labeled = measure.label(seg_map, connectivity=1)
@@ -115,12 +120,13 @@ def segmentation(detection_graph,label_names):
                             box = region.bbox
                             p1 = (box[1], box[0])
                             p2 = (box[3], box[2])
-                            cv2.rectangle(image, p1, p2, (77,255,9), 2)
-                            vis_text(image,label_names[seg_map[tuple(region.coords[0])]],(p1[0],p1[1]-10))
-                cv2.imshow('segmentation',image)
+                            cv2.rectangle(image, p1, p2, (77, 255, 9), 2)
+                            vis_text(image, label_names[seg_map[tuple(region.coords[0])]], (p1[0], p1[1] - 10))
+                cv2.imshow('segmentation', image)
                 if cv2.waitKey(1) & 0xFF == ord('q'):
                     break
                 fps.update()
+                vs.update()
     fps.stop()
     vs.stop()
     cv2.destroyAllWindows()
--- stuff/helper.py
+++ stuff/helper.py
@@ -13,6 +13,7 @@ import tensorflow as tf
 import json
 
 import sys
+
 PY2 = sys.version_info[0] == 2
 PY3 = sys.version_info[0] == 3
 if PY2:
@@ -22,7 +23,7 @@ elif PY3:
 
 
 class FPS:
-# from https://www.pyimagesearch.com/2015/12/21/increasing-webcam-fps-with-python-and-opencv/
+    # from https://www.pyimagesearch.com/2015/12/21/increasing-webcam-fps-with-python-and-opencv/
     def __init__(self):
         # store the start time, end time, and total number of frames
         # that were examined between the start and end intervals
@@ -52,8 +53,8 @@ class FPS:
     def fps(self):
         # compute the (approximate) frames per second
         return self._numFrames / self.elapsed()
-    
-    
+
+
 class FPS2:
     def __init__(self, interval):
         self._glob_start = None
@@ -80,25 +81,25 @@ class FPS2:
         self._glob_numFrames += 1
         self._local_numFrames += 1
         if self.curr_local_elapsed > self._interval:
-          print("> FPS: {}".format(self.fps_local()))
-          self._local_numFrames = 0
-          self._local_start = curr_time
+            print("> FPS: {}".format(self.fps_local()))
+            self._local_numFrames = 0
+            self._local_start = curr_time
 
     def elapsed(self):
         return (self._glob_end - self._glob_start).total_seconds()
 
     def fps(self):
         return self._glob_numFrames / self.elapsed()
-    
+
     def fps_local(self):
         if self.first:
-            return round(self._local_numFrames / self.curr_local_elapsed,1)
+            return round(self._local_numFrames / self.curr_local_elapsed, 1)
         else:
             return 0.0
-    
-    
+
+
 class WebcamVideoStream:
-# with modifications from https://www.pyimagesearch.com/2015/12/21/increasing-webcam-fps-with-python-and-opencv/
+    # with modifications from https://www.pyimagesearch.com/2015/12/21/increasing-webcam-fps-with-python-and-opencv/
     def __init__(self, src, width, height):
         # initialize the video camera stream and read the first frame
         # from the stream
@@ -112,11 +113,11 @@ class WebcamVideoStream:
         # initialize the variable used to indicate if the thread should
         # be stopped
         self.stopped = False
-        #Debug stream shape
+        # Debug stream shape
         self.real_width = int(self.stream.get(3))
         self.real_height = int(self.stream.get(4))
-        print("> Start video stream with shape: {},{}".format(self.real_width,self.real_height))
-    
+        print("> Start video stream with shape: {},{}".format(self.real_width, self.real_height))
+
     def start(self):
         # start the thread to read frames from the video stream
         threading.Thread(target=self.update, args=()).start()
@@ -124,15 +125,15 @@ class WebcamVideoStream:
 
     def update(self):
         # keep looping infinitely until the thread is stopped
-        while True:
-            # if the thread indicator variable is set, stop the thread
-            if self.stopped:
-                self.stream.release()
-                return
+        # while True:
+        # if the thread indicator variable is set, stop the thread
+        if self.stopped:
+            self.stream.release()
+            return
 
-            # otherwise, read the next frame from the stream
-            (self.grabbed, self.frame) = self.stream.read()
-            self.frame_counter += 1
+        # otherwise, read the next frame from the stream
+        (self.grabbed, self.frame) = self.stream.read()
+        self.frame_counter += 1
 
     def read(self):
         # return the frame most recently read
@@ -141,66 +142,65 @@ class WebcamVideoStream:
     def stop(self):
         # indicate that the thread should be stopped
         self.stopped = True
-        
+
     def isActive(self):
         # check if VideoCapture is still Opened
         return self.stream.isOpened
 
     def resize(self):
         try:
-            self.frame = cv2.resize(self.frame, (self.width, self.height)) 
+            self.frame = cv2.resize(self.frame, (self.width, self.height))
         except:
             print("> Error resizing video stream")
-        
 
 
 class SessionWorker():
-# from https://github.com/naisy/realtime_object_detection/blob/master/lib/session_worker.py
-# TensorFlow Session Thread
-#
-# usage:
-# before:
-#     results = sess.run([opt1,opt2],feed_dict={input_x:x,input_y:y})
-# after:
-#     opts = [opt1,opt2]
-#     feeds = {input_x:x,input_y:y}
-#     woker = SessionWorker("TAG",graph,config)
-#     worker.put_sess_queue(opts,feeds)
-#     q = worker.get_result_queue()
-#     if q is None:
-#         continue
-#     results = q['results']
-#     extras = q['extras']
-#
-# extras: None or frame image data for draw. GPU detection thread doesn't wait result. Therefore, keep frame image data if you want to draw detection result boxes on image.
-#
-    def __init__(self,tag,graph,config):
+    # from https://github.com/naisy/realtime_object_detection/blob/master/lib/session_worker.py
+    # TensorFlow Session Thread
+    #
+    # usage:
+    # before:
+    #     results = sess.run([opt1,opt2],feed_dict={input_x:x,input_y:y})
+    # after:
+    #     opts = [opt1,opt2]
+    #     feeds = {input_x:x,input_y:y}
+    #     woker = SessionWorker("TAG",graph,config)
+    #     worker.put_sess_queue(opts,feeds)
+    #     q = worker.get_result_queue()
+    #     if q is None:
+    #         continue
+    #     results = q['results']
+    #     extras = q['extras']
+    #
+    # extras: None or frame image data for draw. GPU detection thread doesn't wait result. Therefore, keep frame image data if you want to draw detection result boxes on image.
+    #
+    def __init__(self, tag, graph, config):
         self.lock = threading.Lock()
         self.sess_queue = Queue.Queue()
         self.result_queue = Queue.Queue()
         self.tag = tag
-        t = threading.Thread(target=self.execution,args=(graph,config))
+        t = threading.Thread(target=self.execution, args=(graph, config))
         t.setDaemon(True)
         t.start()
         return
 
-    def execution(self,graph,config):
+    def execution(self, graph, config):
         self.is_thread_running = True
         try:
-            with tf.Session(graph=graph,config=config) as sess:
+            with tf.Session(graph=graph, config=config) as sess:
                 while self.is_thread_running:
-                        while not self.sess_queue.empty():
-                            q = self.sess_queue.get(block=False)
-                            opts = q["opts"]
-                            feeds= q["feeds"]
-                            extras= q["extras"]
-                            if feeds is None:
-                                results = sess.run(opts)
-                            else:
-                                results = sess.run(opts,feed_dict=feeds)
-                            self.result_queue.put({"results":results,"extras":extras})
-                            self.sess_queue.task_done()
-                        time.sleep(0.005)
+                    while not self.sess_queue.empty():
+                        q = self.sess_queue.get(block=False)
+                        opts = q["opts"]
+                        feeds = q["feeds"]
+                        extras = q["extras"]
+                        if feeds is None:
+                            results = sess.run(opts)
+                        else:
+                            results = sess.run(opts, feed_dict=feeds)
+                        self.result_queue.put({"results": results, "extras": extras})
+                        self.sess_queue.task_done()
+                    time.sleep(0.005)
         except:
             import traceback
             traceback.print_exc()
@@ -213,8 +213,8 @@ class SessionWorker():
         else:
             return False
 
-    def put_sess_queue(self,opts,feeds=None,extras=None):
-        self.sess_queue.put({"opts":opts,"feeds":feeds,"extras":extras})
+    def put_sess_queue(self, opts, feeds=None, extras=None):
+        self.sess_queue.put({"opts": opts, "feeds": feeds, "extras": extras})
         return
 
     def is_result_empty(self):
@@ -231,13 +231,14 @@ class SessionWorker():
         return result
 
     def stop(self):
-        self.is_thread_running=False
+        self.is_thread_running = False
         with self.lock:
             while not self.sess_queue.empty():
                 q = self.sess_queue.get(block=False)
                 self.sess_queue.task_done()
         return
-        
+
+
 class TimeLiner:
     _timeline_dict = None
 
@@ -256,4 +257,4 @@ class TimeLiner:
 
     def save(self, f_name):
         with open(f_name, 'w') as f:
-			json.dump(self._timeline_dict, f)
+            json.dump(self._timeline_dict, f)
